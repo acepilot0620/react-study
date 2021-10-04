3d66f969c681605a0ee3f5a39ce9fdf56afedd89
최정호 <acepilot06200620@gmail.com>
2021-10-04 22:37:47 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Hello from "./Hello";
+// import Wrapper from './Wrapper';
+// import Counter from './counter';
+import InputSample from './InputSample';
+import './App.css';
+
+function App() {
+  
+  return (
+    <InputSample/>
+  );
+}
+
+export default App;
